efc4f2785a4584b9cbbd199d61142165f364dc9e
spoliv <spoliv@rambler.ru>
2014-12-13 19:09:50 +0300

    Commit sec  13.12.2014

--- models/news.php
+++ models/news.php
@@ -4,7 +4,5 @@ $basaone = new DB('localhost', 'root', '', 'test');
 $basaone -> Add_news('news', $_POST['title'], $_POST['text']);
 $basaone -> All_news('news');
 var_dump($ret);
-$news = $ret;
-
-$basaone -> News_getAll('news');
+$news = $basaone -> News_getAll('news');
 
